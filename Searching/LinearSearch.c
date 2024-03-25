c621e5d284b6e273325317136b4147980b4a11ec
ras41 <rs2krs3@gmail.com>
2024-03-25 17:39:12 +0530

    Searching algorithm

---
+++ Searching/LinearSearch.c
@@ -0,0 +1,14 @@
+// Linear Search algorithm by user input
+
+#include <stdio.h>
+
+int main()
+{
+    int a[5], i, found = 0, x; 
+    printf("Enter the elements in Array : ");
+    
+    for(i = 0; i < 5; i++)
+    {
+        scanf("%d", a[i] );
+    }
+}
